3a4a5173b0676887b8d29fc1f409bd80259d0e47
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-01-04 09:29:27 +0000

    users and profile

--- Plantar/admin.py
+++ Plantar/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group,User
+from . import models
 
-# Register your models here.
+
+
+
+class ProfileInline(admin.TabularInline):
+    model = models.Profile
+    extra = 0
+    min_num = 1
+    max_num = 10
+    
+
+class customUser(admin.ModelAdmin):
+    moedl = User
+    fields = ['username']
+    inlines = [ProfileInline]
+    
+    
+admin.site.unregister(Group)
+admin.site.unregister(User)
+admin.site.register(User,customUser)
+
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id','user']
