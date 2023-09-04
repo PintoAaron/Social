037eb5ad01e92b92b81ceee1fa5edc2f7a91672f
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-09-04 17:55:20 +0000

    modified urls

--- Plantar/urls.py
+++ Plantar/urls.py
@@ -1,11 +1,12 @@
+from django.views.generic import TemplateView
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/',views.hello,name="hello"),
-    path('home/',views.home,name="home"),
-    path('profiles/',views.profile_list,name='profile_list'),
-    path('profiles/<int:id>',views.profile,name ="profile"),
-    path('login/',views.login_user,name = "login"),
-    path('logout',views.logout_user,name="logout")
+    path('', views.home, name="home"),
+    path('hello/', views.hello, name="hello"),
+    path('profiles/', views.profile_list, name='profile_list'),
+    path('profiles/<int:id>', views.profile, name="profile"),
+    path('login/', views.login_user, name="login"),
+    path('logout', views.logout_user, name="logout")
 ]
